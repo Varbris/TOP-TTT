d8365d2921a7ebf0f3c91e55d97ce26a5cfe09ff
varbris <92345143+Varbris@users.noreply.github.com>
2025-06-04 14:33:56 +0700

    Fix check winner function
    atleast per row is still checking the winner
    issue: winner per column check and diagonal check

--- javascript.js
+++ javascript.js
@@ -15,16 +15,59 @@ function titatoBoard() {
       console.log("your input row or column is beyond 2");
       return false;
     }
+
     const isCellNotFilled =
       board[row][column].getCellValue() === " " ? true : false;
 
     if (isCellNotFilled) {
       board[row][column].changeCellValue(playerMark);
+      userWinCheck(board);
       return true;
     } else {
       console.log("cell is filled");
       return false;
     }
+  };
+
+  const userWinCheck = function (board) {
+    const rowWinX = ["0X", "1X", "2X"];
+    const rowWinO = ["0O", "1O", "2O"];
+    let winner = "";
+    printBoard();
+    //loop for checking is the array contain X, and if the X exist return the col index
+    let checkX = board.map(function (row, rowIndex) {
+      return row.map(function (col, colIndex) {
+        if (col.getCellValue() === "X") {
+          return colIndex + "X";
+        } else if (col.getCellValue() === "O") {
+          return colIndex + "O";
+        }
+      });
+    });
+
+    //loop for, after check X is exist, and return col index in new array from above
+    // check if any row contain same data with win condition
+    const checkWinner = function (checkX) {
+      console.log(checkX);
+      let checkforX = checkX
+        .map(function (perArray) {
+          if (perArray.toString() === rowWinX.toString()) {
+            winner = "x";
+            return true;
+          } else if (perArray.toString() === rowWinO.toString()) {
+            winner = "o";
+            return true;
+          } else {
+            return false;
+          }
+        })
+        .filter(function (item) {
+          return item === true;
+        });
+
+      return checkforX;
+    };
+    console.log("awerikaower", checkWinner(checkX), winner);
   };
 
   const printBoard = function () {
